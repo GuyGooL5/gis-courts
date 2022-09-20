5b14dcb9097ce9b932c42ec2ec34901942dafc69
Guy Tsitsiashvili <guygool4@gmail.com>
2022-09-20 21:29:55 +0300

    feat(autocomplete): add autocomplete page

--- src/GeomapPage.tsx
+++ src/GeomapPage.tsx
@@ -1,31 +1,54 @@
-import { Card, styled, TextField } from "@mui/material";
-import { Container, Stack } from "@mui/system";
-import GovMapEmbed from "./components/GovMapEmbed";
+import { Box, Card, styled, Switch, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+import { useState } from "react";
+import AutocompleteCourt from "./components/AutocompleteCourt";
+import GovMapEmbed, { RendererType } from "./components/GovMapEmbed";
 import Navbar from "./Navbar";
 import CourtsService from "./services/CourtsService";
 
-const GeomapPage = () => (
-  <Stack
-    direction="column"
-    sx={{ position: "absolute", width: "100%", height: "100%" }}
-  >
-    <Navbar />
-    <SearchCard>
-      <TextField label="Search" />
-      <TextField label="Search" />
-      <TextField label="Search" />
-      <TextField label="Search" />
-      <TextField label="Search" />
-    </SearchCard>
-    <GovMapEmbed court={CourtsService.getCourts()[1]} />
-  </Stack>
-);
+const GeomapPage = () => {
+  const [activeCourt, setActiveCourt] = useState(CourtsService.getCourts()[0]);
+  const [layer, setLayer] = useState<RendererType>(RendererType.Simple);
+  return (
+    <Stack
+      direction="column"
+      sx={{ position: "absolute", width: "100%", height: "100%" }}
+    >
+      <Navbar />
+      <SearchCard dir="rtl">
+        <Box sx={{ mb: 1 }}>
+          <Typography variant="body2" color="text.secondary">
+            בחירת שכבה
+          </Typography>
+          <Typography>
+            תצלום אוויר
+            <Switch
+              color="default"
+              checked={layer === RendererType.SimplePicture}
+              onChange={(e) =>
+                setLayer(
+                  e.target.checked
+                    ? RendererType.SimplePicture
+                    : RendererType.Simple
+                )
+              }
+            />
+            מפה
+          </Typography>
+        </Box>
+        <AutocompleteCourt onSelectCourt={setActiveCourt} />
+      </SearchCard>
+      <GovMapEmbed court={activeCourt} layer={layer} depth="9" />
+    </Stack>
+  );
+};
 
 export default GeomapPage;
 
 const SearchCard = styled(Card)`
   position: fixed;
   padding: ${({ theme }) => theme.spacing(1)};
+  padding-top: ${({ theme }) => theme.spacing(2)};
   top: ${({ theme }) => theme.spacing(10)};
   left: ${({ theme }) => theme.spacing(8)};
 `;
--- src/components/AutocompleteCourt.tsx
+++ src/components/AutocompleteCourt.tsx
@@ -1,5 +1,77 @@
-interface AutocompleteCourtProps {}
+import { useMemo, useState } from "react";
+import {
+  Autocomplete,
+  createFilterOptions,
+  ListItem,
+  TextField,
+  Typography,
+} from "@mui/material";
 
-const AutocompleteCourt = (props: AutocompleteCourtProps) => <div></div>;
+import CourtsService, { Court } from "$/services/CourtsService";
+import { Stack } from "@mui/system";
+import { Place } from "@mui/icons-material";
 
+interface AutocompleteCourtProps {
+  onSelectCourt?: (court: Court) => void;
+}
+
+const AutocompleteCourt = ({ onSelectCourt }: AutocompleteCourtProps) => {
+  const options = useMemo(() => CourtsService.getCourts(), []);
+
+  const filterOptions = createFilterOptions({
+    matchFrom: "any",
+    stringify: ({ data }: Court) =>
+      data.name + " " + data.address + " " + data.city,
+  });
+
+  return (
+    <Autocomplete
+      sx={{ width: 360 }}
+      options={options}
+      getOptionLabel={(option) => option.data.name}
+      filterOptions={filterOptions}
+      renderOption={(props, option) => (
+        <RenderOption {...props} option={option} />
+      )}
+      onChange={(e, value) => value && onSelectCourt?.(value)}
+      renderInput={(params) => (
+        <TextField dir="rtl" {...params} label="בחר בית משפט" />
+      )}
+    />
+  );
+};
 export default AutocompleteCourt;
+
+const RenderOption = ({
+  option: { data },
+  ...props
+}: { option: Court } & React.HTMLAttributes<HTMLLIElement>) => {
+  const removeLastPart = (delimiter: string) => (str: string) => {
+    const split = str.split(delimiter);
+    if (split.length > 1) {
+      split.pop();
+      return split.join(delimiter);
+    }
+    return str;
+  };
+
+  return (
+    <ListItem {...props} dir="rtl">
+      <Stack
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="center"
+        gap={1}
+        textAlign="start"
+      >
+        <Place htmlColor="#3f51b5" />
+        <div>
+          <Typography variant="body1">
+            {removeLastPart(" - ")(data.name)}
+          </Typography>
+          <Typography variant="body2">{data.city}</Typography>
+        </div>
+      </Stack>
+    </ListItem>
+  );
+};
